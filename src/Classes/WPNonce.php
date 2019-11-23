afac635eb7c9fa63507ec00647dc4595d835a858
root <louisdebenito@gmail.com>
2019-11-23 09:53:23 +0100

    addNonceToForm function added

--- src/Classes/WPNonce.php
+++ src/Classes/WPNonce.php
@@ -9,34 +9,48 @@ namespace luisdeb\Woncer\Classes;
  * It implements original WordPress funtions as per the doc files.
  *
  * @see https://codex.wordpress.org/Wordpress_Nonce_Implementation
- *
  */
 class WPNonce
 {
-
+    /**
+     * The current WordPress create nonce function name.
+     *
+     * @var string CREATE_NONCE_FUNCTION_NAME
+     */
     const CREATE_NONCE_FUNCTION_NAME = 'wp_create_nonce';
 
+    /**
+     * The current WordPress nonce URL function name.
+     *
+     * @var string NONCE_URL_FUNCTION_NAME
+     */
     const NONCE_URL_FUNCTION_NAME = 'wp_nonce_url';
 
+    /**
+     * The current WordPress nonce URL function name.
+     *
+     * @var string NONCE_FIELD_FUNCTION_NAME
+     */
     const NONCE_FIELD_FUNCTION_NAME = 'wp_nonce_field';
+
+    const DEFAULT_NONCE_NAME = '_wpnonce';
 
     private $action;
 
-    private $elementId;
-
     private $name;
-
-    private $url;
 
     private $token;
 
-    public function __construct(string $action, string $elementId, string $name)
+    public function __construct(string $action, string $name)
     {
-        if (!empty($action) && !empty($elementId)) {
+        if (trim($name) === "") {
+            $name = DEFAULT_NONCE_NAME;
+        }
+
+        if (isset($action) && trim($action) !== "") {
             $this->setAction($action);
-            $this->setElementId($elementId);
             $this->setName($name);
-            $this->setNonceToken($this->action.$this->elementId);
+            $this->setNonceToken($this->action);
         }
     }
 
@@ -55,30 +69,54 @@ class WPNonce
         $this->name = $name;
     }
 
-    private function setNonceToken(string $actionFull)
+    private function setNonceToken(string $action)
     {
-        $this->token = createNonceToken($actionFull);
+        $this->token = createNonceToken($action);
     }
 
-    private function createNonceToken(string $actionFull): string
+    private function createNonceToken(string $action): string
     {
         $result = "";
 
         if (function_exists(self::CREATE_NONCE_FUNCTION_NAME)) {
-            $result = wp_create_nonce($actionFull);
+            $result = wp_create_nonce($action);
         }
 
         return $result;
     }
 
-    private function addNonceToUrl(string $baseUrl, string $actionFull, string $name=""): string
-    {
+    private function addNonceUrl(
+        string $actionUrl,
+        string $action = null,
+        string $name = null
+    ): string {
+
         $nonceUrl = "";
+        $nonceAction = (!$action) ? $this->action : $action;
+        $nonceName = (!$name) ? $this->name : $name;
 
         if (function_exists(self::NONCE_URL_FUNCTION_NAME)) {
-            $nonceUrl = wp_nonce_url($baseUrl, $actionFull);
+            $nonceUrl = wp_nonce_url($actionUrl, $nonceAction, $nonceName);
         }
 
         return $nonceUrl;
+    }
+
+    private function addNonceToForm(
+        string $action = null,
+        string $name = null,
+        bool $referer = true,
+        bool $echo = true
+    ): string {
+
+        $nonceField = "";
+        $nonceAction = (!$action) ? $this->action : $action;
+        $nonceName = (!$name) ? $this->name : $name;
+
+        if (function_exists(self::NONCE_FIELD_FUNCTION_NAME)) {
+            $nonceField = wp_nonce_field($nonceAction, $nonceName, $referer, $echo);
+        }
+
+        return $nonceField;
     }
 }
