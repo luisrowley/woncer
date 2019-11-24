d556a2c7f83e2ed9cde5a5d7379594989d7f3a1e
root <louisdebenito@gmail.com>
2019-11-24 22:36:42 +0100

    WPNonceChecker Test class added.

---
+++ tests/PHPUnit/WPNonceCheckerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+use Brain\Monkey\Functions as MonkeyFunctions;
+
+use luisdeb\Woncer\Main\WPNonceChecker;
+
+use luisdeb\Woncer\Main\WPNonceFactory;
+
+/**
+ * This class Implements the Unit Test methodology for detecting bugs.
+ *
+ * PHPUnit version 8.0 * 
+ * @see https://phpunit.de/getting-started/phpunit-8.html
+ *
+ */
+class WPNonceCheckerTest extends TestCase
+{
+    
+}
+
+
+?>
