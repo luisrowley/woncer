b03fe6b1560d1fc1fdad09d953ae82421aeb6be8
lewiss <lewiss@vbox.vbox>
2020-01-06 17:57:40 +0100

    test create from request with mock http parameters

--- tests/PHPUnit/WPNonceCreatorTest.php
+++ tests/PHPUnit/WPNonceCreatorTest.php
@@ -8,9 +8,11 @@ use PHPUnit\Framework\TestCase;
 
 use Brain\Monkey\Functions as MonkeyFunctions;
 
-use luisdeb\Woncer\Main\WPNonce;
+use luisdeb\Woncer\Main\WPNonceCreator;
 
 use luisdeb\Woncer\Main\WPNonceFactory;
+
+use luisdeb\Woncer\Main\WPContextRequester;
 
 /**
  * This class Implements the Unit Test methodology for the WPNonce class.
@@ -19,27 +21,35 @@ use luisdeb\Woncer\Main\WPNonceFactory;
  * @see https://phpunit.de/getting-started/phpunit-8.html
  *
  */
-class WPNonceTest extends TestCase
+class WPNonceCreatorTest extends TestCase
 {
     /**
      * Unit tests for WPNonce::addNonceUrl() method.
      *
      * covers @method WPNonceFactory::createDefault
-     * covers @method WPNonce::__construct
-     * covers @method WPNonce::setAction
-     * covers @method WPNonce::setName
-     * covers @method WPNonce::setNonceToken
-     * covers @method WPNonce::createNonceToken
-     * covers @method WPNonce::addNonceUrl
+     * covers @method WPNonceCreator::__construct
+     * covers @method WPNonceCreator::setAction
+     * covers @method WPNonceCreator::setName
+     * covers @method WPNonceCreator::setNonceToken
+     * covers @method WPNonceCreator::createFromRequest
      *
      * @return void
      */
-    public function testUrl()
+    public function testCreateFromRequest()
     {
-        $actionUrl = 'http://www.somewpdomain.com';
         $wpNonceFactory = new WPNonceFactory();
-        MonkeyFunctions\expect(WPNonce::CREATE_NONCE_FUNCTION_NAME)
+        $mockRequest = $this->getMockBuilder(WPContextRequester::class)
+             ->disableOriginalConstructor()
+             ->getMock();
+
+        $mockRequest->httpRequest = array("_wpnonce" => "bar", "action" => "foo");
+        $mockRequest->httpMethod = 'POST';
+
+        $wpNonceCreator = $wpNonceFactory->createDefaultChecker();
+
+        MonkeyFunctions\expect(WPNonceCreator::CREATE_NONCE_FUNCTION_NAME)
             ->once();
+        
         $wpNonce = $wpNonceFactory->createDefault();
         $result = $wpNonce->addNonceUrl($actionUrl);
         $this->assertEmpty($result);
@@ -48,34 +58,5 @@ class WPNonceTest extends TestCase
             ->andReturn($actionUrl . '?escaped=with-nonce-action');
         $result = $wpNonce->addNonceUrl($actionUrl);
         $this->assertStringStartsWith('http', $result);
-    }
-
-    /**
-     * Tests WPNonce::addNonceToForm() method.
-     *
-     * covers @method WPNonceFactory::createDefault
-     * covers @method WPNonce::__construct
-     * covers @method WPNonce::setAction
-     * covers @method WPNonce::setName
-     * covers @method WPNonce::setNonceToken
-     * covers @method WPNonce::createNonceToken
-     * covers @method WPNonce::addNonceToForm
-     *
-     * @return void
-     */
-    public function testForm()
-    {
-        $wpField = '<input type="hidden" id="_wpnonce" name="_wpnonce" value="nonce" />';
-        $wpNonceFactory = new WPNonceFactory();
-        MonkeyFunctions\expect(WPNonce::CREATE_NONCE_FUNCTION_NAME)
-            ->once();
-        $wpNonce = $wpNonceFactory->createDefault();
-        $result = $wpNonce->addNonceToForm();
-        $this->assertEmpty($result);
-        MonkeyFunctions\expect(WPNonce::NONCE_FIELD_FUNCTION_NAME)
-            ->once()
-            ->andReturn($wpField);
-        $result = $wpNonce->addNonceToForm();
-        $this->assertEquals($wpField, $result);
     }
 }
