7d04e05d922c5213eab412e7eb27d45871cf3155
lewiss <louisdebenito@gmail.com>
2020-01-06 20:49:03 +0100

    tested 'nonce' form field with referer set to true

--- tests/PHPUnit/WPNonceFieldCreatorTest.php
+++ tests/PHPUnit/WPNonceFieldCreatorTest.php
@@ -8,7 +8,7 @@ use PHPUnit\Framework\TestCase;
 
 use Brain\Monkey\Functions as MonkeyFunctions;
 
-use luisdeb\Woncer\Main\WPNonce;
+use luisdeb\Woncer\Main\WPNonceFieldCreator;
 
 use luisdeb\Woncer\Main\WPNonceFactory;
 
@@ -19,37 +19,8 @@ use luisdeb\Woncer\Main\WPNonceFactory;
  * @see https://phpunit.de/getting-started/phpunit-8.html
  *
  */
-class WPNonceTest extends TestCase
+class WPNonceFieldCreatorTest extends TestCase
 {
-    /**
-     * Unit tests for WPNonce::addNonceUrl() method.
-     *
-     * covers @method WPNonceFactory::createDefault
-     * covers @method WPNonce::__construct
-     * covers @method WPNonce::setAction
-     * covers @method WPNonce::setName
-     * covers @method WPNonce::setNonceToken
-     * covers @method WPNonce::createNonceToken
-     * covers @method WPNonce::addNonceUrl
-     *
-     * @return void
-     */
-    public function testUrl()
-    {
-        $actionUrl = 'http://www.somewpdomain.com';
-        $wpNonceFactory = new WPNonceFactory();
-        MonkeyFunctions\expect(WPNonce::CREATE_NONCE_FUNCTION_NAME)
-            ->once();
-        $wpNonce = $wpNonceFactory->createDefault();
-        $result = $wpNonce->addNonceUrl($actionUrl);
-        $this->assertEmpty($result);
-        MonkeyFunctions\expect(WPNonce::NONCE_URL_FUNCTION_NAME)
-            ->once()
-            ->andReturn($actionUrl . '?escaped=with-nonce-action');
-        $result = $wpNonce->addNonceUrl($actionUrl);
-        $this->assertStringStartsWith('http', $result);
-    }
-
     /**
      * Tests WPNonce::addNonceToForm() method.
      *
@@ -64,18 +35,32 @@ class WPNonceTest extends TestCase
      * @return void
      */
     public function testForm()
-    {
+    {        
+        $nonceName = "_wpnonce";
+        $nonceAction = "-1";
+        $tokenParam = "nonce";
         $wpField = '<input type="hidden" id="_wpnonce" name="_wpnonce" value="nonce" />';
-        $wpNonceFactory = new WPNonceFactory();
-        MonkeyFunctions\expect(WPNonce::CREATE_NONCE_FUNCTION_NAME)
-            ->once();
-        $wpNonce = $wpNonceFactory->createDefault();
-        $result = $wpNonce->addNonceToForm();
-        $this->assertEmpty($result);
-        MonkeyFunctions\expect(WPNonce::NONCE_FIELD_FUNCTION_NAME)
+        $referer = '<input type="hidden" name="_wp_http_referer" value="http://dev.wordpress.org" />';
+
+        $wpNonceFieldCreator = new WPNonceFieldCreator($nonceAction, $nonceName);
+
+        MonkeyFunctions\expect(WPNonceFieldCreator::CREATE_NONCE_FUNCTION_NAME)
+            ->once()
+            ->andReturn($tokenParam);
+
+        MonkeyFunctions\expect('esc_attr')
+        ->twice()
+        ->andReturn($nonceName, $tokenParam);
+
+        MonkeyFunctions\expect('wp_referer_field')
+        ->once()
+        ->andReturn($referer);
+
+        MonkeyFunctions\expect('addNonceToForm')
             ->once()
             ->andReturn($wpField);
-        $result = $wpNonce->addNonceToForm();
-        $this->assertEquals($wpField, $result);
+
+        $result = $wpNonceFieldCreator->addNonceToForm();
+        $this->assertEquals($wpField . $referer, $result);
     }
 }
